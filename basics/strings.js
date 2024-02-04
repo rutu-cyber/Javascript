8398f9b245e995655de4ae08cac2391443f16c9c
Rutuja Vishwas Devkar <64886246+rutu-cyber@users.noreply.github.com>
2024-02-04 09:43:00 +0000

    strings done

---
+++ basics/strings.js
@@ -0,0 +1,14 @@
+// declaration
+const name = 'rutu'
+const repoCount = 15
+
+// console.log(name + repoCount); // op- rutu15 not good practice
+
+const gameName = new String('deathcharm');
+// console.log(gameName[0]); // op d
+// console.log(gameName.__proto__); // op {}
+
+// console.log(gameName.length); //op 10
+// console.log(gameName.anchor); // deprecated no longer used
+// console.log(gameName.charAt(4)); // op - h
+console.log(gameName.indexOf('t')); // op-3
